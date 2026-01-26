d97fc5216000ee6bbfe50be705a356bfa670ed27
Hamid Oukaamouch <hamidkaamouch123@gmail.com>
2026-01-26 17:34:39 +0100

    final logic for LIS

--- long_inc_sub.c
+++ long_inc_sub.c
@@ -19,6 +19,42 @@ int	binary_search(int tails[], int value, int size)
 	return (start);
 }
 
+void	reverse_lis(int lis[], int len)
+{
+	int	i;
+	int	tmp;
+
+	i = 0;
+	while (i < len / 2)
+	{
+		tmp = lis[i];
+		lis[i] = lis[len - i - 1];
+		lis[len - i - 1] = tmp;
+		i++;
+	}
+}
+
+void	build_lis(int arr[], int lis[], int parent[], int last_index)
+{
+	int	i;
+
+	i = 0;
+	while (last_index != -1)
+	{
+		lis[i] = arr[last_index];
+		last_index = parent[last_index];
+		i++;
+	}
+	reverse_lis(lis, i);
+}
+
+void	lis_init(int *tails_indx, int *parent, int *lis_len)
+{
+	tails_indx[0] = 0;
+	parent[0] = -1;
+	*lis_len = 1;
+}
+
 void	lis_handle(int arr[], int size, int lis[], int *lis_len)
 {
 	int tails[size];
@@ -26,57 +62,24 @@ void	lis_handle(int arr[], int size, int lis[], int *lis_len)
 	int parent[size];
 	int	pos;
 	int	i;
+	int	last_index;
 
-	tails_indx[0] = 0;
-	parent[0] = -1;
+	lis_init(tails_indx, parent, lis_len);
 	tails[0] = arr[0];
-	*lis_len = 1;
 	i = 1;
 	while(i < size)
 	{
-		if(arr[i] > tails[*lis_len - 1])
-		{
-			tails[*lis_len] = arr[i];
-			tails_indx[*lis_len] = i;
-			parent[i] = tails_indx[*lis_len - 1];
-			*lis_len += 1;
-		}
+		pos = binary_search(tails, arr[i], *lis_len);
+		tails[pos] = arr[i];
+		tails_indx[pos] = i;
+		if (pos > 0)
+			parent[i] = tails_indx[pos - 1];
 		else
-		{
-			pos = binary_search(tails, arr[i], *lis_len);
-			tails[pos] = arr[i];
-			tails_indx[pos] = i;
-			if (pos > 0)
-				parent[i] = tails_indx[pos - 1];
-			else
-				parent[i] = -1;
-		}
+			parent[i] = -1;
+		if (*lis_len == pos)
+			(*lis_len)++;
 		i++;
 	}
-	int last_index = tails_indx[*lis_len - 1];
-	int i = 0;
-	while (last_index != -1)
-	{
-		lis[i] = arr[last_index];
-		last_index = parent[last_index];
-		i++;
-	}
-	for(int i = 0; i < (*lis_len) / 2; i++)
-	{
-		int tmp = lis[i];
-		lis[i] = lis[*lis_len - i - 1];
-		lis[*lis_len - i - 1] = tmp;
-	}
+	last_index = tails_indx[*lis_len - 1];
+	build_lis(arr, lis, parent, last_index);
 }
-/*
-int main()
-{
-	int arr[] = {27, 24, 58, 79, 41, 13, 49, 32, 10, 53};
-	int lis[10];
-	int lis_len;
-
-	lis_handle(arr, 10, lis, &lis_len);
-	//for(int i = 0; i < lis_len; i++)
-		//printf("%d ",lis[i]);
-}
-*/
