9659ce5c3c0240845445c4027479f7de5341d646
Hamid Oukaamouch <hamidkaamouch123@gmail.com>
2026-01-26 16:13:04 +0100

    refector the logic of lis

--- lis.c
+++ lis.c
@@ -19,36 +19,26 @@ int	binary_search(int lis[], int size, int target)
 	return start;
 }
 
-void	longest_increasing_subsequence(int arr[], int size)
+void	long_inc_subs(int arr[], int size, int lis[], int *lis_len)
 {
-	int	lis[size];
-	int	lis_len;
 	int	i;
 	int	pos;
 
 	lis[0] = arr[0];
-	lis_len = 1;
+	*lis_len = 1;
 	i = 1;
 	while(i < size)
 	{
-		if (arr[i] >= lis[lis_len - 1])
+		if (arr[i] > lis[*lis_len - 1])
 		{
-			lis[lis_len] = arr[i];
-			lis_len++;
+			lis[*lis_len] = arr[i];
+			*lis_len += 1;
 		}
 		else
 		{
-			pos = binary_search(lis, lis_len, arr[i]);
+			pos = binary_search(lis, *lis_len, arr[i]);
 			lis[pos] = arr[i];
 		}
 	 	i++;
 	}
-	for(int i=0;i<lis_len;i++)
-		printf("%d ",lis[i]);
-}
-
-int main()
-{
-	int arr[]= {10,9,2,5,3,7,101,18};
-	longest_increasing_subsequence(arr,8);
 }
