7eb1764a40435dcd0777ba158693157e5830d28f
Hamid Oukaamouch <hamidkaamouch123@gmail.com>
2026-02-02 13:22:37 +0100

    add bonus header

--- push_swap_bonus.h
+++ push_swap_bonus.h
@@ -6,7 +6,7 @@
 /*   By: houkaamo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/31 13:26:58 by houkaamo          #+#    #+#             */
-/*   Updated: 2026/01/31 14:25:00 by houkaamo         ###   ########.fr       */
+/*   Updated: 2026/02/02 12:29:29 by houkaamo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ typedef struct s_lis
 t_node	*parse_args(int ac, char **av);
 char	**ft_split(const char *s, char c);
 char	*ft_strjoin(const char *s1, char *s2);
+char	*ft_strchr(const char *s, int c);
 int		ft_atoi(const char *s);
 void	free_split(char **arr);
 void	free_stack(t_node *stack);
@@ -50,6 +51,7 @@ int		add_stack_back(t_node **stack, int value);
 void	error_exit(t_node *stack, char **arr);
 void	print_stack(t_node *stack);
 int		is_sorted(t_node *stack);
+int		ft_strcmp(const char *s1, const char *s2);
 
 void	swap(t_node *stack);
 void	sa(t_node *a);
