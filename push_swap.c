113aabebe080c7adef8e8247e0d6c2b9faabe262
Hamid Oukaamouch <hamidkaamouch123@gmail.com>
2026-02-02 18:39:36 +0100

    fix norminette

--- push_swap.c
+++ push_swap.c
@@ -6,43 +6,36 @@
 /*   By: houkaamo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 13:20:27 by houkaamo          #+#    #+#             */
-/*   Updated: 2026/01/28 12:28:27 by houkaamo         ###   ########.fr       */
+/*   Updated: 2026/02/02 18:39:13 by houkaamo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
 	t_node	*a;
 	t_node	*b;
-	int	size;
+	int		size;
 
 	if (ac == 1)
-		return 0;
-	a = parse_args(ac,av);
+		return (0);
+	a = parse_args(ac, av);
 	b = NULL;
-
-	//printf("stack A: Before\n");
-	//print_stack(a);
 	size = stack_size(a);
 	if (size == 1)
-		return 0;
+		return (0);
 	if (is_sorted(a))
-		return 0;
+		return (free_stack(a), 0);
 	if (size == 2)
 		sa(a);
-	else if (size > 2 && size <=	5)
+	else if (size > 2 && size <= 5)
 	{
 		index_stack_element(a);
 		sort_small_stack(&a, &b, size);
 	}
 	else
 		sort_large_stack(&a, &b, size);
-
-	//printf("stack A: After sorted:\n");
-	//print_stack(a);
 	free_stack(a);
-	free_stack(b);
 	return (0);
 }
